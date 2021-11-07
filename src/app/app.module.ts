49fa5abec3c428d9aba93573e185cec63473dc60
amarqueze <alanmarquez@outlook.com>
2021-11-07 18:05:58 +0000

    created shared components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterFormComponent } from './landing/register-form/register-form.component';
+import { LoginFormComponent } from './landing/login-form/login-form.component';
+import { LandingComponent } from './landing/landing.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InputSearchComponent } from './shared/components/input-search/input-search.component';
+import { PokemonCardComponent } from './shared/components/pokemon-card/pokemon-card.component';
+import { PokemonDetailComponent } from './shared/components/pokemon-detail/pokemon-detail.component';
+import { ProfileComponent } from './shared/components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterFormComponent,
+    LoginFormComponent,
+    LandingComponent,
+    DashboardComponent,
+    InputSearchComponent,
+    PokemonCardComponent,
+    PokemonDetailComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
+  ],
+  exports: [
+    InputSearchComponent,
+    PokemonCardComponent,
+    PokemonDetailComponent,
+    ProfileComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
