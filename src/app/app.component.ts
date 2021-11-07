5d096f6532915f1303256a2a16793f15b0df79b2
amarqueze <alanmarquez@outlook.com>
2021-11-07 18:03:16 +0000

    configured app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'pokedex';
+  hasSession = true;
+
+  changeToLanding(): void {
+    this.hasSession = false;
+  }
 }
