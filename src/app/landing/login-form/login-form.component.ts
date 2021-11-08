eab70635ede26cf0ad11fac9d94c5c709d8c05f0
amarqueze <alanmarquez@outlook.com>
2021-11-08 05:39:57 +0000

    Implemented loginForm

--- src/app/landing/login-form/login-form.component.ts
+++ src/app/landing/login-form/login-form.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms'
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html'
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+  @Output() submit: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get email() {
+    return this.loginForm.get('email');
   }
+  
+  get password() {
+    return this.loginForm.get('password');
+  } 
 
+  onSubmit() {
+    this.submit.emit({ email: this.email?.value, password: this.password?.value });
+  }
 }
